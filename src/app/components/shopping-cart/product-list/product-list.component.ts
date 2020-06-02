3efefa6a37d4c07d01742582a38eae29b8c6fe01
ThiliniDBandara <thilinidinushikabandara@gmail.com>
2020-06-02 11:26:15 +0530

    Addtowishlist

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 
 import { Product } from 'src/app/models/product';
+import { WishlistService } from 'src/app/services/wishlist.service';
 
 @Component({
   selector: 'app-product-list',
@@ -11,13 +12,23 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
 
   productList: Product[] = []
+  wishlist: number[] = []
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private wishlistService: WishlistService) { }
 
   ngOnInit(): void {
+   this.loadProducts();
+   this.loadWishlist();
+  }
+  loadProducts(){
     this.productService.getProducts().subscribe((Product) => {
       this.productList = Product;
     });
   }
-
+  loadWishlist(){
+    this.wishlistService.getWishlist().subscribe((productIds) => {
+      console.log(productIds)
+    this.wishlist = productIds
+    })}
 }
